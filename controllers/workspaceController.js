c941f0f038629f00ba69fa97bc029b0a8268622f
Akansha1313 <ak.akanshamishra@gmail.com>
2026-07-23 23:58:56 +0530

    added member invite api

--- controllers/workspaceController.js
+++ controllers/workspaceController.js
@@ -41,7 +41,8 @@ export const getWorkspaces = async (req,res)=>{
 }
 export  const getWorkspacesById = async(req,res)=>{
     try{
-      const workspace = await Workspace.findById (req.params.id);
+      const workspace = await Workspace.findById (req.params.id)
+      .populate('members','name email');
       if(!workspace){
         return res.status(404).json({
             message:"workspace not found"
@@ -56,6 +57,42 @@ export  const getWorkspacesById = async(req,res)=>{
         res.status(500).json({
             message:"error fetching workspaces",
             error:error.message
+        });
+    }
+};
+export const addMemberToWorkspace = async(req,res)=>{
+    const{id}=req.params;
+    const{email}=req.body;
+    try{
+        const workspace = await  Workspace.findById(id);
+        if(!workspace){
+            return res.status(400).json({"message":"workspace not found"});
+          }
+          if(workspace.owner.toString()!==req.user._id.toString()){
+             return res.status(403).json({ message: "Only the workspace owner can invite members" });
+        }
+          const userToInvite = await User.findOne({ email });
+        if (!userToInvite) {
+            return res.status(404).json({ message: "User not found with this email" });
+        }
+          if (workspace.members.some(m => m.toString() === userToInvite._id.toString())) {
+            return res.status(400).json({ message: "User is already a member of this workspace" });
+        }
+        // Add member and save
+        workspace.members.push(userToInvite._id);
+        await workspace.save();
+        res.status(200).json({
+            message: "Member added successfully",
+            member: {
+                _id: userToInvite._id,
+                name: userToInvite.name,
+                email: userToInvite.email
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error adding member",
+            error: error.message
         });
     }
 };
--- routes/WorkspaceRoutes.js
+++ routes/WorkspaceRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createWorkspace ,getWorkspaces,getWorkspacesById} from '../controllers/workspaceController.js';
+import { createWorkspace ,getWorkspaces,getWorkspacesById,addMemberToWorkspace } from '../controllers/workspaceController.js';
 import protect from '../middleware/authMiddleware.js';
 
 const router= express.Router();
@@ -7,4 +7,5 @@ const router= express.Router();
 router.post('/',protect,createWorkspace);
 router.get('/',protect,getWorkspaces);
 router.get('/:id',protect ,getWorkspacesById);
+router.post('/:id/invite',protect,addMemberToWorkspace );
 export default router;
